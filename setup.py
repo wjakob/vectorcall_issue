wjakob/pytest_issue

example project

setup.py
@@ -1,0 +1,10 @@
+from setuptools import Extension, setup
+
+setup(
+    ext_modules=[
+        Extension(
+            name="inheritance_issue",
+            sources=["inheritance_issue.c"],
+        ),
+    ]
+)

wjakob/vectorcall_issue

setup.py
@@ -1,0 +1,10 @@
+from setuptools import Extension, setup
+
+setup(
+    ext_modules=[
+        Extension(
+            name="vectorcall_issue",
+            sources=["vectorcall_issue.c"],
+        ),
+    ]
+)